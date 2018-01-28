e46a86433a0c87a5920d2202d98811ee69103a76
ganeshbalaji23 <ganeshbalaji23@gmail.com>
2018-01-28 09:13:02 +0530

    Initial commit

---
+++ src/main/java/com/sb/projects/SpringMvcjpaApplication.java
@@ -0,0 +1,12 @@
+package com.sb.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcjpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sb/projects/SpringMvcjpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sb.projects;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcjpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
